e29d8efa2369390f5572a5e32cb86444bd801ce8
马驰 <machi@zjrealtech.com>
2018-02-06 18:53:31 +0800

    定时器

--- src/main/java/com/machi/springbootweb/SpringbootWebApplication.java
+++ src/main/java/com/machi/springbootweb/SpringbootWebApplication.java
@@ -2,8 +2,10 @@ package com.machi.springbootweb;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling
 public class SpringbootWebApplication {
 
 	public static void main(String[] args) {
